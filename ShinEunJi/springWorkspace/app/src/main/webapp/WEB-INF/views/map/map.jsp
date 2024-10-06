984336f2e47445f4a14ed8c894c5a48e4320754c
Eun Ji Shin <159417728+belovelace@users.noreply.github.com>
2024-10-07 00:38:01 +0900

    여러개의 마커와 커스텀오버레이 구현 완료

--- ShinEunJi/springWorkspace/app/src/main/webapp/WEB-INF/views/map/map.jsp
+++ ShinEunJi/springWorkspace/app/src/main/webapp/WEB-INF/views/map/map.jsp
@@ -8,14 +8,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>지도 시작하기</title>
   <link rel="stylesheet" href="css/map.css">
+
 </head>
 <body>
   <div id="map" style="width:495px;height:395px;"></div>
-
-
-	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=4b53ced3e6fc3b4b589df87e00c1b019"></script>
-
-
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=4b53ced3e6fc3b4b589df87e00c1b019"></script>
 
   <script src="js/map.js"></script>
 
--- ShinEunJi/springWorkspace/app/src/main/webapp/js/map.js
+++ ShinEunJi/springWorkspace/app/src/main/webapp/js/map.js
@@ -27,55 +27,90 @@ function onGeoError(error) {
 }
 
 ////////////////////////카카오 맵 API////////////////////////////////////
-function showMap(latitude, longitude) {
-		var container = document.getElementById('map');
-		var options = {
-			center: new kakao.maps.LatLng(latitude, longitude),
-			level: 3
-		};
+  function showMap(latitude, longitude) {
+            var container = document.getElementById('map');
+            var options = {
+                center: new kakao.maps.LatLng(latitude, longitude),
+                level: 3
+            };
 
-		var map = new kakao.maps.Map(container, options);
+            var map = new kakao.maps.Map(container, options);
 
-		// 마커를 표시할 위치와 title 객체 배열입니다
-        var positions = [
-            {
-                title: '복우물공원',
-                latlng: new kakao.maps.LatLng(37.455081, 127.128220)
-            },
-            {
-                title: '협동공원',
-                latlng: new kakao.maps.LatLng(37.635346, 127.126036)
-            },
-            {
-                title: '텃밭',
-                latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-            },
-            {
-                title: '근린공원',
-                latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+            var imageSrc = 'https://cdn-icons-png.flaticon.com/512/2840/2840372.png', // 마커이미지의 주소입니다
+                imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+                imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+            // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+            // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
+            var positions = [
+                {
+                    content: '<div class="customoverlay">' +
+                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                             '    <span class="title">구의야구공원</span>' +
+                             '  </a>' +
+                             '</div>',
+                    latlng: new kakao.maps.LatLng(37.54699, 127.09598)
+                },
+                {
+                    content: '<div class="customoverlay">' +
+                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                             '    <span class="title">복우물공원</span>' +
+                             '  </a>' +
+                             '</div>',
+                    latlng: new kakao.maps.LatLng(37.455059, 127.128223)
+                },
+                {
+                    content: '<div class="customoverlay">' +
+                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                             '    <span class="title">효창공원</span>' +
+                             '  </a>' +
+                             '</div>',
+                    latlng: new kakao.maps.LatLng(37.544806, 126.959170)
+                },
+                {
+                    content: '<div class="customoverlay">' +
+                             '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+                             '    <span class="title">평화의공원</span>' +
+                             '  </a>' +
+                             '</div>',
+                    latlng: new kakao.maps.LatLng(37.562035, 126.894650)
+                }
+            ];
+
+            for (var i = 0; i < positions.length; i++) {
+                // 마커를 생성합니다
+                var marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: positions[i].latlng, // 마커의 위치
+                    image: markerImage // 커스텀 마커 이미지
+                });
+
+                // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                var content = positions[i].content;
+
+                // 커스텀 오버레이를 생성합니다
+                var customOverlay = new kakao.maps.CustomOverlay({
+                    map: map,
+                    position: positions[i].latlng,
+                    content: content,
+                    yAnchor: 1
+                });
+
+                // 기본적으로 커스텀 오버레이는 지도에 표시됩니다. 필요에 따라 표시/숨김을 제어할 수 있습니다.
+                // 예: 커스텀 오버레이를 클릭 시 토글
+                (function(marker, customOverlay) {
+                    kakao.maps.event.addListener(marker, 'click', function() {
+                        if (customOverlay.getMap()) {
+                            customOverlay.setMap(null);
+                        } else {
+                            customOverlay.setMap(map);
+                        }
+                    });
+                })(marker, customOverlay);
             }
-        ];
-
-        // 마커 이미지의 이미지 주소입니다
-        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-        for (var i = 0; i < positions.length; i ++) {
-
-            // 마커 이미지의 이미지 크기 입니다
-            var imageSize = new kakao.maps.Size(24, 35);
-
-            // 마커 이미지를 생성합니다
-            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-            // 마커를 생성합니다
-            var marker = new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: positions[i].latlng, // 마커를 표시할 위치
-                title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image : markerImage // 마커 이미지
-            });
         }
-}//showmap
 
 
 
